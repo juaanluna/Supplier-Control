ae01bec56d5785985844a84e66541803d621afe2
VonLohengramm <edua123sonic@gmail.com>
2020-08-12 20:06:19 -0300

    Alterado layout da tela de Cadastro de Item

--- src/screens/cadastrarItem/cadastrarItem.js
+++ src/screens/cadastrarItem/cadastrarItem.js
@@ -26,51 +26,54 @@ const CadastrarItem = () => {
     <View style={styles.background}>
       <StatusBar backgroundColor="#B20000" barStyle="light-content" />
       {/* <Text style={styles.title}>Cadastrar Item</Text> */}
-      <View>
-        <Text style={styles.label}>Nome</Text>
-        <AuthInput style={styles.input}
-          placeholder="Nome do Item"
-          placeholderTextColor="#233245"
-          onChangeText={text => setNome(text)}
-          maxLength={30}
-        />
-      </View>
-      <View>
-        <Text style={styles.label}>Categoria</Text>
-        {/* <AuthInput style={styles.input}
-          placeholder="Categoria"
-          placeholderTextColor="#233245"
-          onChangeText={text => setCategoria(text)}
-          maxLength={30}
-        /> */}
-        <View style={styles.picker}>
-          <Picker selectedValue={categoria}
-            onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}>
-            {categorias.map(item => {
-              return (
-                <Picker.Item label={item.nome} value={item.nome} />
-              );
-            }
-            )}
-          </Picker>
-        </View>
-      </View>
-      <View>
-        <Text style={styles.label}>Detalhes</Text>
-        <AuthInput style={styles.inputArea}
-          placeholder="Detalhes"
-          placeholderTextColor="#233245"
-          onChangeText={text => setDetalhes(text.trim())}
-          multiline={true}
-          numberOfLine={4}
-          maxLength={50}
-        />
-      </View>
 
-      <View>
-        <TouchableOpacity style={styles.buttons} onPress={Cadastrar}>
-          <Text>Cadastrar</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.label}>Nome</Text>
+          <AuthInput style={styles.input}
+            placeholder="Nome do Item"
+            placeholderTextColor="#233245"
+            onChangeText={text => setNome(text)}
+            maxLength={30}
+          />
+        </View>
+        <View>
+          <Text style={styles.label}>Categoria</Text>
+          {/* <AuthInput style={styles.input}
+            placeholder="Categoria"
+            placeholderTextColor="#233245"
+            onChangeText={text => setCategoria(text)}
+            maxLength={30}
+          /> */}
+          <View style={styles.picker}>
+            <Picker selectedValue={categoria}
+              onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}>
+              {categorias.map(item => {
+                return (
+                  <Picker.Item key={item.id} label={item.nome} value={item.nome} />
+                );
+              }
+              )}
+            </Picker>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.label}>Detalhes</Text>
+          <AuthInput style={styles.inputArea}
+            placeholder="Detalhes"
+            placeholderTextColor="#233245"
+            onChangeText={text => setDetalhes(text.trim())}
+            multiline={true}
+            numberOfLine={4}
+            maxLength={50}
+          />
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.buttons} onPress={Cadastrar}>
+            <Text>Cadastrar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -83,6 +86,14 @@ const styles = StyleSheet.create({
     fontSize: 25,
     marginBottom: 20,
   },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    backgroundColor: "#5A6978",
+    padding: 10,
+  },  
   buttons: {
     alignItems: "center",
     backgroundColor: "#B20000",
@@ -92,10 +103,11 @@ const styles = StyleSheet.create({
     width: "90%",
   },
   label: {
-    margin: 10
+    margin: 10,
+    color: "#fff"
   },
   background: {
-    backgroundColor: "#AAA",
+    backgroundColor: "#F95F62",
     flex: 1,
     width: "100%",
     alignItems: "center"
@@ -114,7 +126,7 @@ const styles = StyleSheet.create({
   },
   picker: {
     height: 50,
-    width: 300,
+    width: 280,
     borderRadius: 20,
     backgroundColor: "#f2f2f2",
     textAlign: "center",
